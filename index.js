9af35d33a8da8b8b8a2d26df490ab326e6039121
Burak Tamturk <buraktamturk@gmail.com>
2020-03-16 18:53:57 +0200

    fix some issues

--- index.js
+++ index.js
@@ -2,10 +2,19 @@
 const core = require('@actions/core');
 const action = require('./action.js');
 
-const apiKey = core.getInput('secret');
-const destination = core.getInput('destination');
+const apiKey = core.getInput('secret', { required: true });
+const destination = core.getInput('destination', { required: true });
 const format = core.getInput('format');
+if(!format || format === '') {
+    format = 'json';
+}
+
 const failIfNotChanged = core.getInput('failIfNotChanged');
+if(!failIfNotChanged || failIfNotChanged === '') {
+    failIfNotChanged = false;
+} else {
+    failIfNotChanged = !!failIfNotChanged;
+}
 
 action({ apiKey, destination, format, failIfNotChanged })
     .then(function(count) {
